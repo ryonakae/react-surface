20967a69a4d70f085caabaebaa6a7c311befbd59
Kasper Sandin <kasper.sandin@gmail.com>
2018-01-06 00:39:08 +0100

    Add ValueFunction feature to Tween

--- src/lib/tween/Tween.ts
+++ src/lib/tween/Tween.ts
@@ -2,26 +2,43 @@ import {getArithmetic, defaultOptions} from './settings';
 import {TweenOptions} from './TweenOptions';
 import TweenInstruction, {TweenInstructionProps} from './TweenInstruction';
 import {TweenSugar} from './TweenSugar';
+import {IArithmetic} from './arithmetics/IArithmetic';
+
 const TWEEN = require('tween.js');
 
 let idCounter = 0;
 
+type ValueFunction<TValue> = () => TValue;
+
 export class Tween<TValue> {
   public readonly id = idCounter += 1;
 
+  private valueFunction?: ValueFunction<TValue>;
+  private intrinsicValue?: TValue;
   private internalTween?: TWEEN.Tween;
   private resolvers: Array<(value: TValue) => void> = [];
+  private arithmetic: IArithmetic<TValue>;
+  private sugar = new TweenSugar(this.options);
 
   lastInstruction: TweenInstruction<TValue>;
 
   constructor (
-    public value: TValue, 
+    value: TValue | ValueFunction<TValue>,
     public options: TweenOptions = defaultOptions
-  ) {}
-  
-  // TODO make sure arithmetic isn't changed
-  get arithmetic () {
-    return getArithmetic(this.value);
+  ) {
+    if (typeof value === 'function') {
+      this.valueFunction = value;
+    } else {
+      this.intrinsicValue = value;
+      this.arithmetic = getArithmetic(value);
+    }
+  }
+
+  get value () {
+    if (this.valueFunction) {
+      return this.valueFunction();
+    }
+    return this.intrinsicValue;
   }
 
   private onTweenCompleted () {
@@ -48,7 +65,7 @@ export class Tween<TValue> {
     this.stop();
 
     if (inst.from !== undefined) {
-      this.value = inst.from;
+      this.intrinsicValue = inst.from;
     }
 
     return new Promise<TValue>((resolve) => {
@@ -58,7 +75,7 @@ export class Tween<TValue> {
 
       if (inst.speed !== undefined) {
         const delta = this.arithmetic.abs(
-          this.arithmetic.subtract(inst.to, this.value)
+          this.arithmetic.subtract(inst.to, this.intrinsicValue)
         );
         const numberOfFrames = this.arithmetic.maxDivide(delta, inst.speed);
         duration = 16 * numberOfFrames;
@@ -72,7 +89,7 @@ export class Tween<TValue> {
       this.internalTween = new TWEEN.Tween(tweenTarget);
       (this.internalTween as any).__owner = this;
 
-      const startValue = this.value;
+      const startValue = this.intrinsicValue;
       const delta = this.arithmetic.subtract(inst.to, startValue);
 
       this.internalTween!
@@ -80,7 +97,7 @@ export class Tween<TValue> {
         .delay(inst.options!.delay)
         .to({progress: 1}, duration)
         .onUpdate(() => {
-          this.value = this.arithmetic.parse(
+          this.intrinsicValue = this.arithmetic.parse(
             this.arithmetic.add(
               startValue,
               this.arithmetic.multiply(delta, tweenTarget.progress)
@@ -95,23 +112,23 @@ export class Tween<TValue> {
   // Syntax sugar for common tween instructions
 
   set (value: TValue) {
-    this.value = value;
+    this.intrinsicValue = value;
   }
 
   toggle (offValue: TValue, onValue: TValue, isOn: boolean, options?: TweenOptions) {
-    return this.instruct(TweenSugar.default.toggle(offValue, onValue, isOn, options));
+    return this.instruct(this.sugar.toggle(offValue, onValue, isOn, options));
   }
 
   to (to: TValue, props: Partial<TweenInstructionProps<TValue>> = {}) {
-    return this.instruct(TweenSugar.default.to(to, props));
+    return this.instruct(this.sugar.to(to, props));
   }
 
   from (from: TValue, props: Partial<TweenInstructionProps<TValue>> = {}) {
-    return this.instruct(TweenSugar.default.from(from, props));
+    return this.instruct(this.sugar.from(from, props));
   }
 
   transition (from: TValue, to: TValue, props: Partial<TweenInstructionProps<TValue>> = {}) {
-    return this.instruct(TweenSugar.default.transition(from, to, props));
+    return this.instruct(this.sugar.transition(from, to, props));
   }
 
   static update (): Tween<any>[] {
