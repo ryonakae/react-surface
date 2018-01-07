895d5c86c4e396c936aa60fe2ae8dea0b3b6b743
Kasper Sandin <kasper.sandin@gmail.com>
2018-01-07 03:57:02 +0100

    Create Interactive component

--- demo/src/ui/Button.tsx
+++ demo/src/ui/Button.tsx
@@ -2,40 +2,20 @@ import * as React from 'react';
 import {interaction} from 'pixi.js';
 import * as Color from 'color';
 import {observer} from 'mobx-react/custom';
-import {observable, action} from 'mobx';
 import {tweenSugar} from './UISettings';
 import {grid} from './UISettings';
+import {Interactive} from './Interactive';
 
 @observer
 export class Button extends React.Component<{
   label: string,
   onClick?: (e: interaction.InteractionEvent) => void;
 }> {
-  @observable isHovered = false;
-  @observable isActive = false;
-
-  @action
-  setActive (isActive: boolean) {
-    this.isActive = isActive;
-  }
-
-  @action
-  setHovered (isHovered: boolean) {
-    this.isHovered = isHovered;
-  }
-
   render () {
     return (
-      <surface
-        {...styles.button(this.isHovered, this.isActive)}
-        onMouseDown={this.setActive.bind(this, true)}
-        onMouseUp={this.setActive.bind(this, false)}
-        onMouseEnter={this.setHovered.bind(this, true)}
-        onMouseLeave={this.setHovered.bind(this, false)}
-        onClick={this.props.onClick}
-      >
+      <Interactive style={styles.button} onClick={this.props.onClick}>
         {this.props.label}
-      </surface>
+      </Interactive>
     );
   }
 }
---
+++ demo/src/ui/Interactive.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import {interaction} from 'pixi.js';
+import {observer} from 'mobx-react/custom';
+import {observable, action} from 'mobx';
+
+@observer
+export class Interactive extends React.Component<
+  SurfaceProps & {
+  children?: SurfaceProps['children'] | ((isHovered: boolean, isActive: boolean) => SurfaceProps['children']);
+  style?: (isHovered: boolean, isActive: boolean) => SurfaceStyle;
+}> {
+  @observable isHovered = false;
+  @observable isActive = false;
+
+  static defaultProps = {
+    children: (): null => null,
+    style: (): null => null
+  };
+
+  @action
+  setActive (isActive: boolean) {
+    this.isActive = isActive;
+  }
+
+  @action
+  setHovered (isHovered: boolean) {
+    this.isHovered = isHovered;
+  }
+
+  render () {
+    const {style, children, onMouseDown, onMouseUp, onMouseEnter, onMouseLeave, ...rest} = this.props;
+    const evaluatedChildren = typeof children === 'function' ?
+      children(this.isHovered, this.isActive) :
+      children;
+
+    return (
+      <surface
+        {...style(this.isHovered, this.isActive)}
+        {...rest}
+        onMouseDown={joinFn(this.setActive.bind(this, true), onMouseDown)}
+        onMouseUp={joinFn(this.setActive.bind(this, false), onMouseUp)}
+        onMouseEnter={joinFn(this.setHovered.bind(this, true), onMouseEnter)}
+        onMouseLeave={joinFn(this.setHovered.bind(this, false), onMouseLeave)}
+      >
+        {evaluatedChildren}
+      </surface>
+    );
+  }
+}
+
+function joinFn (a: any, b: any): any {
+  if (a && b) {
+    return () => [a(), b()];
+  }
+  return a || b;
+}
